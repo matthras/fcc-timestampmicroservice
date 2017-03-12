5d322b910d67ab0d1ee83241b779214eedded70c
matthras <matthew.y.p.mack@gmail.com>
2017-03-12 13:12:59 +1100

    added comments and object to return

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ var express = require('express');
 var app = express();
 
 app.get('/:timestring', function(req,res) {
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var timestring = req.params.timestring;
-  // Check timestring to see if it's a number or not using parseInt. If the string has words/non-numbers in it, it'll be 'NaN
-  if(!isNaN(parseInt(timestring))) {
-    console.log("It's a number!")
+  var timenumber = parseInt(timestring); // If timestring is not a number, timenumber = NaN
+  var returnObject = {};
+  // Checking the input to see if it's NaN or not.
+  if(!isNaN(timenumber)) {
+    returnObject.unix = timenumber;
+    // Construct date from Unix number, multiply by 1000 because it's in milliseconds.
+    var date = new Date(timenumber*1000);
+    var naturalString = months[date.getMonth()] + " " + date.getUTCDate() + ", " + date.getFullYear();
+    returnObject.natural = naturalString; 
   } else {
+    // Since timestring is not a number, we need to check if it has a month name in it.
     console.log("It's definitely not a number.")
   }
-  res.send('Hello World!')
+  res.send(returnObject)
 })
 
 app.listen(8080, console.log('Listening on port 8080'))
