5d459240a79c8df2213f6879d602a8d9031ea3ba
matthras <matthew.y.p.mack@gmail.com>
2017-03-12 13:12:31 +1100

    added algorithm to determine if parameter is UNIX timestamp

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+// Test URLs
+// localhost:8080/1450137600
+// localhost:8080/December%2015,%202015
+// Expected Output
+// { "unix": 1450137600, "natural": "December 15, 2015" }
+
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
+app.get('/:timestring', function(req,res) {
+  var timestring = req.params.timestring;
+  // Check timestring to see if it's a number or not using parseInt. If the string has words/non-numbers in it, it'll be 'NaN
+  if(!isNaN(parseInt(timestring))) {
+    console.log("It's a number!")
+  } else {
+    console.log("It's definitely not a number.")
+  }
   res.send('Hello World!')
 })
 
